dcd05d59e998c2f17bcdc224a544de664fdef824
mjasim <mahmood.jasim.bd@gmail.com>
2018-09-12 11:28:27 -0400

    hw01 and lab01 added

---
+++ lab/lab01/tests/q332.py
@@ -0,0 +1,33 @@
+test = {
+  'name': '3.3.2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # It looks like you didn't change the cell to define
+          >>> # x_td_linear appropriately.  It should be a number,
+          >>> # computed using Python's arithmetic.  
+          >>> x_td_linear != ...
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> x_td_linear
+          1700
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ lab/lab01/tests/q52.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '5.2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> biggest_distance_m
+          0.478
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
