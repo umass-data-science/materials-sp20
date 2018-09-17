06a5ee7aa73e41edc0862104014043492feeb5cc
mjasim <mahmood.jasim.bd@gmail.com>
2018-09-17 12:46:14 -0400

    adding solution for lab 01

---
+++ solution/lab/lab01/tests/q331.py
@@ -0,0 +1,33 @@
+test = {
+  'name': '3.3.1.',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # It looks like you didn't change the cell to define
+          >>> # x_td appropriately.  It should be a number,
+          >>> # computed using Python's arithmetic.  
+          >>> x_td != ...
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> x_td
+          209715200
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
