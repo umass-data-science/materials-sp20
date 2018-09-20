896c30cb192dc5d57d6d5b575cd53712fffb4267
mjasim <mahmood.jasim.bd@gmail.com>
2018-09-19 20:19:32 -0400

    adding hw02 and lab02

---
+++ lab/lab02/tests/q211.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> welcome
+          'Welcome to 190F: Foundations of Data Science!'
+           """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
