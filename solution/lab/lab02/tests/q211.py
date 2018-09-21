8d08b0d3d7a42d608f9273cfdaccaa80776474ee
mjasim <mahmood.jasim.bd@gmail.com>
2018-09-21 13:04:43 -0400

    added lab02 solutions

---
+++ solution/lab/lab02/tests/q211.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '2.1.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> welcome
+          'Welcome to 190F: Foundations of Data Science!'
+           """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
