cf3b48139d957756c6bd69e267e7c74f4624f683
mjasim <mahmood.jasim.bd@gmail.com>
2018-10-03 17:27:09 -0400

    adding lab04 and hw04

---
+++ hw/hw04/tests/q1_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> chapter_lengths
+          array([1005, 2016, 1613, 4382])
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ hw/hw04/tests/q1_3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> character_count("The quick(!) brown fox jumped over the lazy(?) dog.")
+          40
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
