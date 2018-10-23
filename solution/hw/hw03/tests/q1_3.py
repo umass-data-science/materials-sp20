0280134c722b85e2d5ddd2108fb3797e0fa0a138
mjasim <mahmood.jasim.bd@gmail.com>
2018-10-23 19:45:46 -0400

    adding solutions lab05 lab06 hw04 hw05

---
+++ solution/hw/hw03/tests/q1_3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 5 <= revised_dissimilarity <= 9
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
